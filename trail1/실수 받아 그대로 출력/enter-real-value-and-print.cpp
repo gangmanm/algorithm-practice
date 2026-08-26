e7e8fcbe8e0aa2a6d6713e7955db590704188bef
Do Kyung Lee <97601109+gangmanm@users.noreply.github.com>
2026-08-26 17:11:34 +0900

    [Passed] 실수 받아 그대로 출력 (2ms, 0MB)

---
+++ trail1/실수 받아 그대로 출력/enter-real-value-and-print.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+
+    double a;
+
+    cin >> a;
+    cout.precision(2);
+    cout << a << endl;
+
+    return 0;
+}
