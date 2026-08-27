25191deec8875f73e0bf68488c2d61d3679aad6a
Do Kyung Lee <97601109+gangmanm@users.noreply.github.com>
2026-08-27 11:14:31 +0900

    [Passed] 실수 입력받아 계산 2 (2ms, 0MB)

---
+++ trail1/실수 입력받아 계산 2/calculation-by-inputting-a-real-number-2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // Please write your code here.
+    cout << fixed;
+    double a;
+
+    cin >> a;
+    cout.precision(2);
+    cout << a + 1.5 << endl;
+
+    
+    return 0;
+}
